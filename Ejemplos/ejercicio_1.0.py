5cf235940a41073526ddc225ff2f6398e63f67aa
Sandro Encarnacion <89167943+swel94@users.noreply.github.com>
2022-01-25 00:08:21 +0000

    cambios

---
+++ Ejemplos/ejercicio_1.0.py
@@ -0,0 +1,24 @@
+#Hacer un programa que permita digitar 7 personas (nombres) con sus edades.
+#a)Cuantos hay mayores de edades
+#b)Cuantas hay menores de edades
+#c)Mostrar la sumatoria de las edades de los que son menores de edades
+#d)Mostrar las edades mayores que sean pares
+
+lista = []
+
+acumulador = 0
+
+contador1 = 0
+contador2 = 0
+
+for i in range(7):
+    persona = str(input("Dime tu nombre: "))
+    edad = int(input("Dime tu edad: "))
+    if edad >= 18:
+        contador1 = contador1 + 1
+    elif edad < 18:
+        contador2 = contador2 + 1
+        acumulador = acumulador + 1
+        lista.app
+        
+      
