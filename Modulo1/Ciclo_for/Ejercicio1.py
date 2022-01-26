fe270d7b403de06aafa4852b9b6f8a140e3a7c1b
Sandro Encarnacion <89167943+swel94@users.noreply.github.com>
2022-01-26 16:21:18 +0000

    cambios

---
+++ Modulo1/Ciclo_for/Ejercicio1.py
@@ -0,0 +1,4 @@
+#1.Generar los números del 1 al 10.
+
+for i in range(1,11):
+  print(i)
---
+++ Modulo1/Ciclo_for/Ejercicio2.py
@@ -0,0 +1,7 @@
+#2.Pedir al usuario digitar el final y generar los números del 1 hasta ese final.
+
+numero = int(input("Ingrese el final de la secuencia: "))
+
+for i in range(1, numero +1):
+  print(i)
+  
