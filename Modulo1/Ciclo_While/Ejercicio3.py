386688b8180376cfd169fcfec73912d82bd0e3a0
Sandro Encarnacion <89167943+swel94@users.noreply.github.com>
2022-01-31 20:29:39 +0000

    cambios

--- Modulo1/Ciclo_While/Ejercicio3.py
+++ Modulo1/Ciclo_While/Ejercicio3.py
@@ -1,19 +1,30 @@
 #3.Realizar un programa que calcule la cantidad de cervezas pedidas en un bar, mostrándole al cliente si desea comprar otra y tomando como referencia la cantidad de dinero que carga el cliente.
 
-acumulador = 0
+pedidas = 0
+devuelta = 0
 cerveza = 250
-acumulador2 = 3000
 
-pedido = int(input("Ingrese la cantidad de cervezas que desea:  "))
 
-while pedido > 0:
-  acumulador = acumulador + pedido
-  otra = input("Desea otra cerveza? y o n: ")
-  if otra == "y":
-    pedido = int(input("Ingrese la cantidad de cervezas que desea: "))
-  elif otra == "n":
-    print("El total de cervezas son: ", acumulador)
-    print("Gracias por su compra")
+
+cartera = int(input("Cuanto dinero tiene usted?: "))
+
+while cartera >= cerveza:
+  pedidos = input("Desea un cerveza y / n?: ")
+  if pedidos == "y":
+    pedidas = pedidas + 1
+    cartera = cartera - cerveza
+  elif pedidos == "n":
+    print("Gracias por su compra!")
+    break
+
+else:
+  print("Vaya de ahi arrancao!")
+
+print("El total de cervezas bebidas fueron" ,pedidas)
+  
+
+  
+ 
    
 
     
